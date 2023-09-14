50fc3266a20977edc32168554065980c6c04f1fa
abhishek <abhishek.tripathi456@gmail.com>
2023-09-14 13:39:09 +0530

    basic parser * contentName with basic args

--- lib/swift_class.ex
+++ lib/swift_class.ex
@@ -3,16 +3,18 @@ defmodule SwiftClass do
   import NimbleParsec
   import Words
 
-  # input = "font(.largeTitle) bold italic "
-  # output = [["font", [".largeTitle"], nil], ["bold", [true], nil], ["italic", [true], nil]]
-
   classnames =
     repeat(
       ignore(whitespace(min: 0))
       |> concat(word())
       |> concat(attribute())
+      |> concat(content_name())
       |> wrap()
     )
 
+
+  defp append_nil(rest, args, context, _line, _offset) do
+    {rest, args ++ [nil], context}
+  end
   defparsec(:parse, classnames)
 end
--- lib/words.ex
+++ lib/words.ex
@@ -15,9 +15,48 @@ defmodule Words do
     |> reduce({Enum, :join, []})
   end
 
+  def single_quoted_string do
+    ignore(string(~s(')))
+    |> repeat(
+      lookahead_not(ascii_char([?']))
+      |> choice([string(~s(\')), utf8_char([])])
+    )
+    |> ignore(string(~s(')))
+    |> reduce({List, :to_string, []})
+  end
+
+  def double_quoted_string do
+    ignore(string(~s(")))
+    |> repeat(
+      lookahead_not(ascii_char([?"]))
+      |> choice([string(~s(\")), utf8_char([])])
+    )
+    |> ignore(string(~s(")))
+    |> reduce({List, :to_string, []})
+  end
+
   def maybe_brackets do
+    true_value =
+      string("true")
+      |> replace(true)
+
+    false_value =
+      string("false")
+      |> replace(false)
+
     ignore(string("("))
-    |> choice([word(), dotted_word()])
+    |> choice([
+      true_value,
+      false_value,
+      word(),
+      dotted_word(),
+      repeat(
+        ignore(optional(whitespace(min: 1)))
+        |> concat(double_quoted_string())
+        |> ignore(optional(whitespace(min: 1)))
+      ),
+      ignore(whitespace(min: 0))
+    ])
     |> ignore(string(")"))
   end
 
@@ -32,7 +71,21 @@ defmodule Words do
   def standalone_attribute do
     # choice([
     # default value to be true.
-    whitespace(min: 0) |> replace(true)
+    whitespace(min: 1) |> replace(true)
     # ])
+  end
+
+  def content_name do
+    # {:content}
+    valid_content_name =
+      ignore(string("{"))
+      |> concat(ignore(string(":")))
+      |> concat(word())
+      |> ignore(string("}"))
+      |> map({String, :to_atom, []})
+
+    invalid_content_name = ignore(string("")) |> post_traverse({:append_nil, []})
+
+    choice([valid_content_name, invalid_content_name])
   end
 end
